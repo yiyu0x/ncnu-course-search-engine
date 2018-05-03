b6a7c50730b3a3dd0ec5ac6b038d7535e4ccfd34
yiyu0x <tako0903@icloud.com>
2018-05-03 12:32:01 +0800

    rm select option value

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+
 app.use(express.static('public'));
+
 app.listen(3000,function(){
-	console.log("hello");
+	console.log("server start");
+	console.log("http://127.0.0.1:3000");
 });
+
+app.get('/search',function(req,res){
+   console.log("GET request");
+   console.log(req.query.department);
+})
