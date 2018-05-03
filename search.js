268dd4680d9c8ac5984f90036eefabca6edf68b4
yiyu0x <tako0903@icloud.com>
2018-05-03 11:15:49 +0800

    node + express connect to public/html

---
+++ search.js
@@ -0,0 +1,27 @@
+
+var http = require('http');
+var querystring = require('querystring');
+ 
+var postHTML = require('./html')
+ 
+http.createServer(function (req, res) {
+  var body = "";
+  req.on('data', function (chunk) {
+    body += chunk;
+  });
+  req.on('end', function () {
+    // 解析参数
+    body = querystring.parse(body);
+    // 设置响应头部信息及编码
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
+ 
+    if(body.name && body.url) { // 输出提交的数据
+        res.write("网站名：" + body.name);
+        res.write("<br>");
+        res.write("网站 URL：" + body.url);
+    } else {  // 输出表单
+        res.write(postHTML.htmlString);
+    }
+    res.end();
+  });
+}).listen(3000);
