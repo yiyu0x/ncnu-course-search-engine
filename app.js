fb47a5997fe01762d606603be595977e32568cf4
aFish <tako0903@icloud.com>
2018-05-04 17:55:19 +0800

    sql connect and deal user input

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // var quary = require('./quary_exec');
-
+var quary_exec = require('./quary_exec');
 const express = require('express');
 const app = express();
+
+sql_quary = "SELECT * FROM ncnu_info where ";
+
 app.use(express.static('public'));
 
 app.listen(3000,function(){
@@ -10,13 +13,46 @@ app.listen(3000,function(){
 });
 
 app.get('/search',function(req,res){
-   console.log("GET request");
-   console.log(req.query.faculty);
-   console.log(req.query.department);
-   console.log(req.query.division);
-   console.log(req.query.location);
-   // console.log(quary.output);
+	// console.log("GET request");
+	// console.log(req.query.faculty);
+	// console.log(req.query.department);
+	// console.log(req.query.division);
+	// console.log(req.query.course_credit);
+	
+	if (req.query.faculty == "不指定"){
+		req.query.faculty = "%";
+	}
+	if (req.query.department == "不指定"){
+		req.query.department = "%";
+	}
+	if (req.query.credit  == "不指定"){
+		req.query.credit  = "%";
+	}
+	if (req.query.division == "不指定"){
+		req.query.division = "%";
+	}
+	if (req.query.location == "不指定"){
+		req.query.location = "%";
+	}
+	user_faculty       = "'" + req.query.faculty       +"'";
+	user_department    = "'" + req.query.department    +"'";
+	user_course_credit = "'" + req.query.credit        +"'";
+	user_division      = "'" + req.query.division      +"'";
+	user_location      = "'" + req.query.location      +"'";
+
+   	sql_quary += "faculty like "       + user_faculty          + " and " +
+				 "department like "    + user_department       + " and " +
+				 "division like "      + user_division         + " and " +
+				 "course_credit like " + user_course_credit    + " and " +
+				 "location like "      + user_location         + ";";
+
+	console.log("sql_quary : " + sql_quary);
+    quary_exec.searchDB(sql_quary);
+
 })
 
 
-// console.log(quary.output);
+
+
+
+
--- quary_exec.js
+++ quary_exec.js
@@ -1,10 +1,26 @@
 var sqlite3 = require('sqlite3').verbose();
 var file = "src/ncnu.db";
 var db = new sqlite3.Database(file);
-db.all("SELECT * FROM ncnu_info", function(err, rows) {
+
+function searchDB(sql){
+	console.log("hi")
+	db.all(sql, function(err, rows) {
         rows.forEach(function (row) {
+        	console.log("hi")
             console.log(row.faculty);
             console.log(row);
         })
 	});	
-db.close();
+	db.close();
+
+}
+
+// db.all("SELECT * FROM ncnu_info", function(err, rows) {
+//         rows.forEach(function (row) {
+//             console.log(row.faculty);
+//             console.log(row);
+//         })
+// 	});	
+// db.close();
+
+exports.searchDB = searchDB;
