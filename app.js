fed288aa4c8d875b8a99613b5f0f1cb198256585
aFish <tako0903@icloud.com>
2018-05-07 23:25:11 +0800

    fix port forward

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ sql_quary = "SELECT * FROM ncnu_info where ";
 app.use(express.static('public'));
 
 var port = process.env.PORT || config.port;
-app.listen(3000,function(){
+app.listen(port,function(){
 	console.log("server start");
 	console.log("http://127.0.0.1:3000");
 });
