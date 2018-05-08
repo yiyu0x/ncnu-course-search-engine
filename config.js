a0a6644f0e8d76c72ffb44a7a5dfd8e18c920266
aFish <tako0903@icloud.com>
2018-05-08 21:49:22 +0800

    config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// config.js
+var config = {
+    development: {
+        port: 3000,
+        // anything else
+    },
+    production: {
+        port: 3001,
+        // anything else
+    }
+};
+
+module.exports = config;
