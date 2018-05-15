f6e00a4c2c5bce87a1c0167ae3bfd71abd862fb0
OliWave <a0924100192@gmail.com>
2018-05-15 21:07:03 +0800

    font-end repairment support mobile

--- public/shownavcon.js
+++ public/shownavcon.js
@@ -2,5 +2,5 @@ window.onload = function() {
     nav = document.getElementById("nav");
     con = document.getElementById("container");
     nav.style.opacity = 1.0;
-    con.style.opacity = 1.0;
+    con.style.opacity = 1.0;	
 }
