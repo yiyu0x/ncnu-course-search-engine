9dd9671e1b51984fe4df755bdeeacb79e89e2a09
OliWave <a0924100192@gmail.com>
2018-05-16 10:51:30 +0800

    fix the changing page problem by adding a new index_ejs.css file

--- public/shownavcon.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    nav = document.getElementById("nav");
-    con = document.getElementById("container");
-    nav.style.opacity = 1.0;
-    con.style.opacity = 1.0;	
-}
