558fe65b3215e1388d0497f74eb7cb97a9781dc3
aFish <tako0903@icloud.com>
2018-08-24 00:40:45 +0800

    change file mode

--- build_file/ncnu_api_get.py
+++ build_file/ncnu_api_get.py
@@ -6,11 +6,9 @@ URL = "https://api.ncnu.edu.tw/API/get.aspx?xml=course_ncnu&year=107&semester=1&
 print('waiting...')
 
 filepath = "./src/feed.xml"
-if os.path.isfile(filepath):
-    os.remove(filepath) 
-
 response = requests.get(URL)
-with open(filepath, 'w') as file:
+
+with open(filepath, 'wb') as file:
     file.write(response.content)
 
 print('done.')
